0ecce0ddb1e5f885bf0a10a9ecc9066e59204953
Hieu Pham-origin <hieupham112003@gmail.com>
2023-12-17 23:42:35 +0700

    class

--- 52100796_52100818.py
+++ 52100796_52100818.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2 
 import numpy as np
-
+class DetectStudentNumber:
+    def __init__(self, image_path, train_img):
+        self.image_path = image_path
+        self.train_img = train_img
+    def preprocessing(self):
+        return None
+    def boundingBox(self):
+        return None
 # arr1 = np.array([223, 225, 233])
 # arr2 = np.array([123, 456, 789])
 
